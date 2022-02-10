f1e8cfce6f7bf80c7f500eb03b88e11c981c9300
GiangPhanA <daperidot@gmail.com>
2022-02-10 17:18:54 +0700

    add dialog

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +24,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Header() {
+  const [open, setOpen] = useState (false);
+
+  const handleClickOpen = () => {
+    console.log('Da Click')
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const classes = useStyles();
 
   return (
@@ -32,9 +46,27 @@ export default function Header() {
           <Typography variant="h6" className={classes.title}>
             GIANG PHAN
           </Typography>
-          <Button color="inherit">Resgister</Button>
+          <Button color="inherit" onClick={handleClickOpen}>Resgister</Button>
+          
         </Toolbar>
       </AppBar>
+
+      <Dialog disableEscapeKeyDown={true} disableBackdropClick={true}
+      open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+
+        </DialogActions>
+      </Dialog>
+      
     </div>
   );
 }
