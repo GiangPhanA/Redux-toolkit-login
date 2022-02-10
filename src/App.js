83d5dbb47e33cbb82abae7ea037ce390b2bd6d83
GiangPhanA <daperidot@gmail.com>
2022-02-10 15:46:16 +0700

    add menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import CounterFeature from './features/Counter'
 
 function App() {
   return (
     <div className="App">
-      <h1>Giang Phan</h1>
+      <Header />
       <CounterFeature />
     </div>
   );
